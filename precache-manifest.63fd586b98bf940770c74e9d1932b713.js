6f9e48d46f6662e912a75867254cf45017b54b10
John Vidal <ba.john.vidal@gmail.com>
2020-08-25 14:19:16 -0300

    Updates

---
+++ precache-manifest.63fd586b98bf940770c74e9d1932b713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d43ef515d3bb48b887d5857cc2c18f",
+    "url": "/my-franchise/index.html"
+  },
+  {
+    "revision": "70565869b17e88a6aace",
+    "url": "/my-franchise/static/js/2.cd05c861.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-franchise/static/js/2.cd05c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbc01b2802748d59373",
+    "url": "/my-franchise/static/js/main.40fc74f0.chunk.js"
+  },
+  {
+    "revision": "3b0248644aa479c961ca",
+    "url": "/my-franchise/static/js/runtime-main.8c235671.js"
+  }
+]);
