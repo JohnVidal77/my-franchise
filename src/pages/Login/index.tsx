3968ac09ee394b510ade0b76435041c0a25d24f3
John Vidal <ba.john.vidal@gmail.com>
2021-03-24 23:46:36 -0300

    user can register and login (localstorage)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,71 @@
-import React from 'react';
+import React, { useCallback, useState, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import Logo from '../../assets/logo-orange.png';
+import IsValidEmail from '../../services/IsValidEmail';
+import { ToastSuccess, ToastWarning } from '../../services/ShowToaster';
+
+interface IUserData {
+  company: {
+    name: string;
+    doc: string;
+  };
+  name: string;
+  email: string;
+  password: string;
+}
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = useCallback(
+    (e: FormEvent): void => {
+      e.preventDefault();
+
+      if (!email || !IsValidEmail(email)) return ToastWarning('Email missing');
+
+      if (!password) return ToastWarning('Password missing');
+
+      const data = localStorage.getItem('@MyFranchise-User');
+
+      let user;
+
+      if (data) user = JSON.parse(data) as IUserData;
+
+      if (user?.email === email && user?.password === password)
+        return ToastSuccess('Welcome back!');
+
+      return ToastWarning('Email or password wrong');
+    },
+    [email, password],
+  );
+
   return (
     <div className="flex justify-center items-center bg-blue-500 h-screen w-screen">
       <div className="max-w-sm px-1">
         <figure className="mb-8">
           <img src={Logo} alt="My franchise" />
         </figure>
-        <form className="flex flex-col items-center">
-          <Input name="email" placeholder="Email" />
-          <Input name="password" placeholder="Password" />
-          <Button label="LOG IN" />
+        <form onSubmit={handleLogin} className="flex flex-col items-center">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <Button type="submit" label="LOG IN" />
 
           <span className="text-white my-4">
             {"Don't have an account? "}
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useCallback, useState, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import Logo from '../../assets/logo-orange.png';
+import { ToastSuccess, ToastWarning } from '../../services/ShowToaster';
+import IsValidEmail from '../../services/IsValidEmail';
 
 const Register: React.FC = () => {
+  const [companyName, setCompanyName] = useState('');
+  const [companyDoc, setCompanyDoc] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = useCallback(
+    (e: FormEvent): void => {
+      e.preventDefault();
+
+      if (!companyDoc || !companyName || !name || !email || !password)
+        return ToastWarning('You must fill all fields');
+
+      if (!IsValidEmail(email)) return ToastWarning('Must be a valid Email');
+
+      const data = {
+        company: {
+          name: companyName,
+          doc: companyDoc,
+        },
+        name,
+        email,
+        password,
+      };
+
+      localStorage.setItem('@MyFranchise-User', JSON.stringify(data));
+
+      return ToastSuccess('User created!');
+    },
+    [companyDoc, companyName, email, name, password],
+  );
+
   return (
     <div className="relative justify-center items-center bg-blue-500 min-h-screen h-full w-screen">
       <nav className="flex top-0 left-0 right-0 justify-center p-4 mb-4">
@@ -17,6 +51,7 @@ const Register: React.FC = () => {
           <img src={Logo} alt="My franchise" />
         </figure>
       </nav>
+
       <div className="max-w-sm md:max-w-2xl mx-auto px-1">
         <div className="text-center text-white mb-6">
           <h1 className="text-2xl font-semibold">Welcome!</h1>
@@ -25,24 +60,53 @@ const Register: React.FC = () => {
             below.
           </span>
         </div>
-        <form className="flex flex-wrap flex-col md:flex-row text-white">
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-wrap flex-col md:flex-row text-white"
+        >
           <fieldset className="md:w-1/2 md:px-2 mb-4">
             <legend className="mb-2 border-b font-semibold border-white w-full">
               Company info
             </legend>
-            <Input name="companyName" placeholder="Company Name" />
-            <Input name="document" placeholder="Document" />
+            <Input
+              type="text"
+              name="companyName"
+              placeholder="Company Name"
+              onChange={e => setCompanyName(e.target.value)}
+            />
+            <Input
+              type="number"
+              name="document"
+              placeholder="Document"
+              onChange={e => setCompanyDoc(e.target.value)}
+            />
           </fieldset>
           <fieldset className="md:w-1/2 md:px-2 mb-4">
             <legend className="mb-2 border-b font-semibold border-white w-full">
               User info
             </legend>
-            <Input name="name" placeholder="Name" />
-            <Input name="email" placeholder="Email" />
-            <Input name="password" placeholder="Password" />
+            <Input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={e => setName(e.target.value)}
+            />
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={e => setPassword(e.target.value)}
+            />
           </fieldset>
           <div className="w-full mb-4 text-center">
-            <Button label="REGISTER" />
+            <Button type="submit" label="REGISTER" />
           </div>
         </form>
       </div>
